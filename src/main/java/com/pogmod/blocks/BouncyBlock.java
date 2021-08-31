611bc0ff3058347398b0e3f6babb2366e90787e7
Jake Pichugin <jakepichugin@gmail.com>
2021-08-30 21:38:44 -0400

    Finished the mod

--- src/main/java/com/pogmod/blocks/BouncyBlock.java
+++ src/main/java/com/pogmod/blocks/BouncyBlock.java
@@ -5,7 +5,11 @@ import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
@@ -21,15 +25,27 @@ public class BouncyBlock extends Block {
     @Override
     public void stepOn(World world, BlockPos blockpos, Entity entity) {
         super.stepOn(world, blockpos, entity);
-        entity.setDeltaMovement(0, 1.0, 0);
+
+        entity.setDeltaMovement(0.0, 1, 0);
+
+
+
+
+
 
     }
 
     @Override
     public void fallOn(World world, BlockPos blockpos, Entity entity, float falldist) {
-        super.fallOn(world, blockpos, entity, falldist);
-        entity.causeFallDamage(falldist, 0.0F);
+
+        if(entity.isSuppressingBounce()) {
+            super.fallOn(world, blockpos, entity, falldist);
+        }
+        else{
+            entity.causeFallDamage(falldist, 0.0F);
+        }
     }
+
 
 
 
--- src/main/java/com/pogmod/world/biome/BouncyBiome.java
+++ src/main/java/com/pogmod/world/biome/BouncyBiome.java
@@ -3,6 +3,8 @@ package com.pogmod.world.biome;
 import com.pogmod.util.RegistryHandler;
 import net.minecraft.block.Blocks;
 import net.minecraft.world.biome.*;
+import net.minecraft.world.gen.feature.structure.ShipwreckStructure;
+import net.minecraft.world.gen.feature.structure.VillageStructure;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 import net.minecraftforge.common.Tags;
@@ -18,20 +20,26 @@ public class BouncyBiome  {
         final BiomeGenerationSettings.Builder biomeGenBuilder = new BiomeGenerationSettings.Builder().surfaceBuilder(
                 SurfaceBuilder.DEFAULT.configured(new SurfaceBuilderConfig(
                         RegistryHandler.BOUNCY_BLOCK.get().defaultBlockState(),
-                        Blocks.DIRT.defaultBlockState(),
+                        Blocks.SLIME_BLOCK.defaultBlockState(),
                         Blocks.SLIME_BLOCK.defaultBlockState()))
         );
 
         DefaultBiomeFeatures.addDefaultOres(biomeGenBuilder);
         DefaultBiomeFeatures.addFossilDecoration(biomeGenBuilder);
+        DefaultBiomeFeatures.addFossilDecoration(biomeGenBuilder);
+        DefaultBiomeFeatures.addDefaultUndergroundVariety(biomeGenBuilder);
+        DefaultBiomeFeatures.addDefaultOres(biomeGenBuilder);
+        DefaultBiomeFeatures.addDefaultCarvers(biomeGenBuilder);
+        DefaultBiomeFeatures.addJungleTrees(biomeGenBuilder);
+        DefaultBiomeFeatures.addDefaultOverworldOceanStructures(biomeGenBuilder);
 
         return new Biome.Builder()
                 .precipitation(Biome.RainType.NONE)
                 .temperature(0.9F)
                 .downfall(0)
                 .biomeCategory(Biome.Category.PLAINS)
-                .depth(0.4F)
-                .scale(0.5F)
+                .depth(0.2F)
+                .scale(0.3F)
                 .specialEffects(
                         new BiomeAmbience.Builder()
                                 .waterColor(0xc90579)
